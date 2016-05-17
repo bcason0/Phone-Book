bcd017b84063e17fb453ba9bbb418ab020f32f17
Bryant Cason <bryantcason@Zipcoder23s-MacBook-Pro.local>
2016-05-17 17:39:58 -0400

    Fixed lab and added Part1

--- src/main/java/io/bryantcason/PhoneBook.java
+++ src/main/java/io/bryantcason/PhoneBook.java
@@ -6,6 +6,59 @@ import java.util.TreeMap;
 
 public class PhoneBook {
 
+    Map<String, String> contacts = new TreeMap<String, String>();
+
+    public void addContact(String name, String number){
+        contacts.put(name, number);
+    }
+
+    public void removeContact(String name){
+        contacts.remove(name);
+
+    }
+
+    public String lookUpContact(String name){
+        return contacts.get(name);
+    }
+
+    public String listAllContacts(){
+        String str = "";
+        for(Map.Entry<String, String> entry : contacts.entrySet()){
+            String name = entry.getKey();
+            String number = entry.getValue();
+
+            str += name + ", " + number + ", ";
+        }
+        System.out.println(str);
+        return str;
+    }
+
+    public String listAllNames(){
+        String str = "";
+        for(Map.Entry<String, String> entry : contacts.entrySet()){
+            String name = entry.getKey();
+            str += name + ", ";
+        }
+        System.out.println(str);
+        return str;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     Map<String, Person> personMap;
 
     public PhoneBook(){
@@ -20,8 +73,13 @@ public class PhoneBook {
         personMap.remove(name);
     }
 
+    public void reverseLookUp(){
+
+    }
+
     public Person lookUpPerson(String name){
         return personMap.get(name);
     }
+*/
 
 }
--- src/test/java/io/bryantcason/PhoneBookSpec.java
+++ src/test/java/io/bryantcason/PhoneBookSpec.java
@@ -1,11 +1,70 @@
 package io.bryantcason;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PhoneBookSpec {
 
+    PhoneBook newarkPhoneBook;
+
+    @Before
+    public void init() {
+        newarkPhoneBook = new PhoneBook();
+        newarkPhoneBook.addContact("Jet Li", "555.555.5555");
+        newarkPhoneBook.addContact("Jackie Chan", "4953");
+        newarkPhoneBook.addContact("Bruce Lee", "02542");
+    }
+
+    @Test
+    public void lookUpContactTest(){
+        String expectedValue = "555.555.5555";
+        String actualValue = newarkPhoneBook.lookUpContact("Jet Li");
+        assertEquals("Show the number for Jet Li", expectedValue, actualValue);
+    }
+
+    @Test
+    public void listAllNamesTest(){
+        String expectedValue = "Bruce Lee, Jackie Chan, Jet Li, ";
+        String actualValue = newarkPhoneBook.listAllNames();
+        assertEquals("Should list all names", expectedValue, actualValue);
+    }
+
+    @Test
+    public void listAllContactsTest(){
+        String expectedValue = "Bruce Lee, 02542, Jackie Chan, 4953, Jet Li, 555.555.5555, ";
+        String actualValue = newarkPhoneBook.listAllContacts();
+        assertEquals("Names and numbers should show", expectedValue, actualValue);
+    }
+
+    @Test
+    public void removeContacts(){
+        newarkPhoneBook.removeContact("Jet Li");
+        String actualValue = newarkPhoneBook.lookUpContact("Jet Li");
+        assertNull("Should return null", actualValue);
+        newarkPhoneBook.listAllContacts();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     @Test
     public void addPersonTest(){
         PhoneBook newarkPhoneBook = new PhoneBook();
@@ -33,5 +92,5 @@ public class PhoneBookSpec {
         String actualPerson = newarkPhoneBook.lookUpPerson("Jet Li").getName();
         assertEquals("This test looks up Jet li",expectedPerson,actualPerson);
     }
-
+*/
 }
