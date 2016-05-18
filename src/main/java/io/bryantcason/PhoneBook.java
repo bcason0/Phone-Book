f09c4abec62233054f942972ff1374e196c233ea
Bryant Cason <bryantcason@Zipcoder23s-MBP.home>
2016-05-17 20:23:10 -0400

    Deleted unwanted code

--- src/main/java/io/bryantcason/PhoneBook.java
+++ src/main/java/io/bryantcason/PhoneBook.java
@@ -53,43 +53,4 @@ public class PhoneBook {
         return str;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    Map<String, Person> personMap;
-
-    public PhoneBook(){
-        personMap = new TreeMap<String, Person>();
-    }
-
-    public void addPerson(String name, Person person){
-        personMap.put(name, person);
-    }
-
-    public void removePerson(String name){
-        personMap.remove(name);
-    }
-
-    public void reverseLookUp(){
-
-    }
-
-    public Person lookUpPerson(String name){
-        return personMap.get(name);
-    }
-*/
-
 }
